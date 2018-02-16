6258c9315ef0ebe4f783d2059a0694279b7ee481
fcc <fcc.felippe@gmail.com>
2018-02-16 17:09:46 -0200

    init repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wiki
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wiki/core_test.clj
@@ -0,0 +1,7 @@
+(ns wiki.core-test
+  (:require [clojure.test :refer :all]
+            [wiki.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
